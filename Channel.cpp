ba72ab9084e1b4d966e2e68c2f49001a1e1c81d5
hgrranzi <uteena@student.42.fr>
2023-12-16 16:45:22 +0100

    Added some member handling in Channel class

--- Channel.cpp
+++ Channel.cpp
@@ -8,3 +8,67 @@ Channel::Channel(const std::string &name) {
 Channel::~Channel() {
     std::cout << "Channel " << this->name << " destroyed." << std::endl;
 }
+
+
+const std::string& Channel::getName() const {
+    return name;
+}
+
+const std::string& Channel::getTopic() const {
+    return topic;
+}
+
+const std::string& Channel::getMode() const {
+    return mode;
+}
+
+const std::set<int>& Channel::getMemberFds() const {
+    return memberFds;
+}
+
+const std::set<int>& Channel::getOperatorFds() const {
+    return operatorFds;
+}
+
+
+void Channel::addMember(int clientFd) {
+    memberFds.insert(clientFd);
+}
+
+void Channel::removeMember(int clientFd) {
+    removeOperator(clientFd);
+    memberFds.erase(clientFd);
+}
+
+bool Channel::hasMebmer(int clientFd) {
+    return memberFds.find(clientFd) != memberFds.end();
+}
+
+
+void Channel::addOperator(int clientFd) {
+    operatorFds.insert(clientFd);
+}
+
+
+void Channel::removeOperator(int clientFd) {
+    operatorFds.erase(clientFd);
+}
+
+bool Channel::hasOperator(int clientFd) {
+    return operatorFds.find(clientFd) != operatorFds.end();
+}
+
+// Channel::void broadcastMessage(int speakerFd, const std::string& message) {
+//     std::string fullMessage = "[" + this->name + "] " + message + "\r\n";
+//     // Broadcast the message to all members
+//     for (int clientFd : clientFds) {
+//         ssize_t bytesSent = send(clientFd, fullMessage.c_str(), fullMessage.length(), 0);
+//         if (bytesSent < fullMessage.length()) {
+//             ;
+//         // Close the connection and remove client
+//         }
+//     }
+
+// }
+
+
