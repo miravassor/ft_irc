6435b5fcad52da7d36c87a59257c6c07c58ebf03
corecaps <corecaps@gmail.com>
2023-12-21 18:13:11 +0100

    Sending reply should be working now

--- Server.cpp
+++ Server.cpp
@@ -1,7 +1,6 @@
 #include "Server.hpp"
 #include "Channel.hpp"
 #include <fcntl.h>
-
 #include <cstring>  // for strerror
 #include <cerrno>   // for errno
 
@@ -12,7 +11,6 @@ Server::Server(int port, const std::string &password) {
     address.sin_port = htons(port);
     // setting the IP - INADDR_ANY for any network interface on the machine - converting it to network byte order.
     address.sin_addr.s_addr = htonl(INADDR_ANY);
-
     // creating the main listening socket and adding it to pollFds container
     socketFd = socket(address.sin_family, SOCK_STREAM | SOCK_NONBLOCK, 0);
 	if (socketFd == -1) {
@@ -20,21 +18,17 @@ Server::Server(int port, const std::string &password) {
     }
     pollfd serverPollFd;
     serverPollFd.fd = socketFd;
-    serverPollFd.events = POLLIN | POLLOUT;
+    serverPollFd.events = POLLIN;
     pollFds.push_back(serverPollFd);
-
     // binding socket to the port
     if (bind(this->socketFd, (sockaddr *) (&address), sizeof(address)) == -1) {
         throw std::runtime_error("Bind error: [" + std::string(strerror(errno)) + "]");
     }
-
     this->start = time(0);
     this->_password = password;
     this->serverName = "42.IRC";
     this->serverVersion = "0.1";
-
 	initCmd();
-
     std::cout << "Server created: address=" << inet_ntoa(address.sin_addr)
 			  << ":"
 			  << ntohs(address.sin_port)
@@ -85,7 +79,6 @@ void Server::removeClient(int clientSocket) {
 
 
 void Server::run() {
-
     listenPort();
     while (true) {
         int countEvents = poll(&pollFds[0], pollFds.size(), 0);
@@ -110,7 +103,6 @@ void Server::run() {
 					std::cout << "Error: " << e.what() << std::endl;
 				}
 				pollFds[i].revents = 0;
-
 			}
 			if (pollFds[i].revents & POLLIN) {
                 // if i == 0 -> first connection
@@ -123,8 +115,7 @@ void Server::run() {
                     if (bytesRead > 0) {
                         _buffer[bytesRead] = 0;
                         if (parsBuffer(pollFds[i].fd)) {
-//                            removeClient(pollFds[i].fd);
-//                            i--;
+//                            TODO : handle parsing errors
 							(void) 0;
                         }
                     } else if (bytesRead == 0) {
@@ -161,26 +152,21 @@ int Server::acceptConnection() {
         throw std::runtime_error("ERROR! Cannot accept the connection");
     }
 	int flags = fcntl(socketFd, F_GETFL, 0);
-
 	if (flags == -1) {
 		// Handle error
 		throw std::runtime_error("Failed to get socket flags");
 	}
-
 	if (fcntl(socketFd, F_SETFL, flags | O_NONBLOCK) == -1) {
 		// Handle error
 		throw std::runtime_error("Failed to set socket to non-blocking mode");
 	}
-
 	clientPollFd.fd = clientSocket;
     clientPollFd.events = POLLIN;
     pollFds.push_back(clientPollFd);
-
     // print information about the accepted connection
     std::cout << "Accepted connection from: " << inet_ntoa(clientAddress.sin_addr) << ":"
               << ntohs(clientAddress.sin_port)
               << " at fd=" << clientSocket << std::endl;
-
     return clientSocket;
 }
 
